26b0692449073f1477cef4ef62902a7750a73882
N H Quynh Trang <131467351+Traysii@users.noreply.github.com>
2023-08-24 16:32:32 +0700

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('',TaskList.as_view(), name= 'list'),
-    path('login', Login.as_view(), name= 'login'),
-    path('logout', LogoutView.as_view(next_page = 'login'), name= 'logout'),
-    path('register', Register.as_view(), name= 'register'),
+    path('login/', Login.as_view(), name= 'login'),
+    path('logout/', LogoutView.as_view(next_page = 'login'), name= 'logout'),
+    path('register/', Register.as_view(), name= 'register'),
     path('task_detail/<int:pk>', TaskDetail.as_view(), name= 'detail'),
     path('create_task',TaskCreate.as_view(), name= 'create'),
     path('update_task/<int:pk>', TaskUpdate.as_view(), name= 'update'),
